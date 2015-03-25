f11e0ad7323edbf539bc13e37260572e0d816b87
Victor D. Savariego <victordsavariego@Victors-MacBook-Pro.local>
2015-03-25 14:37:03 -0300

    Initial Commit

---
+++ Draw/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Draw
+//
+//  Created by Victor D. Savariego on 25/3/15.
+//  Copyright (c) 2015 Victor D. Savariego. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
